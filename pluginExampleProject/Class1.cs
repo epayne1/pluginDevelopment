aef468d264b66a542f20f85c71192632ed611861
epayne1 <53873621+epayne1@users.noreply.github.com>
2019-08-07 15:37:43 -0400

    Add files via upload

---
+++ pluginExampleProject/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pluginExampleProject
+{
+    public class Class1
+    {
+    }
+}
